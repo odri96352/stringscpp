cd6db14e1654cd7951cf4673e9a3812a881db643
odri96352 <130649579+odri96352@users.noreply.github.com>
2023-11-28 17:11:29 +0100

    Add files via upload

--- string.h
+++ string.h
@@ -4,9 +4,13 @@ class string{
     string(const string& copy);
     string(char* s);
     string(char* s, int n);
+
     int size();
+    int length();
     void print();
     char* c_str();
+    int max_size();
+
   private :
     char* list_char_;
     int size_;
