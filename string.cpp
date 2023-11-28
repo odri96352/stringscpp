a86955994f042c95724a1c6351849764eab6e15c
mvernier06 <146165128+mvernier06@users.noreply.github.com>
2023-11-28 16:51:14 +0100

    Add files via upload

--- string.cpp
+++ string.cpp
@@ -35,20 +35,13 @@ void string::print(){
   std::cout<<std::endl;
 }
 
-string::string(char* s){
-  //Copies the null-terminated character sequence
-  //(C-string) pointed by s.
-  this -> list_char_ = new char[1];
-  list_char_[0] = *s;
-  this -> size_ = 1;
-};
+char* string::c_str(){
 
-string::string(char* s, int n){
-  //Copies the first n characters
-  //from the array of characters pointed by s.
-  this -> size_ = n;
-  this -> list_char_ = new char[this -> size_];
-  for(int i=0; i<this -> size_; i++){
-    list_char_[i] = s[i];
+  char* cEquivalent=new char[this->size_ +1];
+  for(int i=0; i<this->size_; ++i){
+    cEquivalent[i]=this->list_char_[i];
   }
-};
+  cEquivalent[this->size_ +1]='\0';
+  //cEquivalent[this->size_ +2]='0';
+  return cEquivalent;
+}
--- string.h
+++ string.h
@@ -2,10 +2,9 @@ class string{
   public :
     string();
     string(const string& copy);
-    string(char* s);
-    string(char* s, int n);
     int size();
     void print();
+    char* c_str();
   private :
     char* list_char_;
     int size_;
