f359241f6d3da0d4d34346acae6d8a9bcb3ca31a
odri96352 <130649579+odri96352@users.noreply.github.com>
2023-11-28 17:09:35 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,11 +2,35 @@
 #include<cstdlib>
 #include<iostream>
 
+void test_constructor_c_string(char* s);
+void test_constructor_c_string_array(char* s, int size_t);
+int test_max_size();
+
 int main(){
   string test = string();
   test.print();
 
   std::cout<<test.c_str()<<std::endl;
+  
+  char a = 'a';
+  char* s = &a;
+  test_constructor_c_string(s);
+
+  int size_t = 3;
+  char t[] = {'a','b','c'};
+  test_constructor_c_string_array(t, size_t);
 
   std::exit(EXIT_SUCCESS);
 }
+
+void test_constructor_c_string(char* s){
+  string output = string(s);
+  output.print();
+};
+
+void test_constructor_c_string_array(char* t, int size_t){
+  string output = string(t, size_t);
+  output.print();
+};
+
+int test_max_size(){};
--- string.cpp
+++ string.cpp
@@ -9,11 +9,19 @@ string::string(){
   list_char_=new char[size_];
   list_char_[0]=':';
   list_char_[1]=')';
-}
+};
 
 int string::size(){
   return this->size_;
-}
+};
+
+int string::length(){
+  return this->size_;
+};
+
+int string::max_size(){
+  return size_max;
+};
 
 string::string(const string& copy){
   int length=copy.size_;
@@ -25,23 +33,39 @@ string::string(const string& copy){
 	for (int i = 0; i<length; i++){
 	    list_char_[i] = copy.list_char_[i];
 	}
-}
-
+};
 
 void string::print(){
   for(int i=0; i<this->size_; ++i){
     std::cout<<this->list_char_[i];
   }
   std::cout<<std::endl;
-}
+};
+
+string::string(char* s){
+  //Copies the null-terminated character sequence
+  //(C-string) pointed by s.
+  this -> list_char_ = new char[1];
+  list_char_[0] = *s;
+  this -> size_ = 1;
+};
+
+string::string(char* s, int n){
+  //Copies the first n characters
+  //from the array of characters pointed by s.
+  this -> size_ = n;
+  this -> list_char_ = new char[this -> size_];
+  for(int i=0; i<this -> size_; i++){
+    list_char_[i] = s[i];
+  }
+};
 
 char* string::c_str(){
-
   char* cEquivalent=new char[this->size_ +1];
   for(int i=0; i<this->size_; ++i){
     cEquivalent[i]=this->list_char_[i];
-  }
+    }
   cEquivalent[this->size_ +1]='\0';
   //cEquivalent[this->size_ +2]='0';
   return cEquivalent;
-}
+};
