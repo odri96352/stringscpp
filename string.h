3fa545556085bd3cbb5fe8651de4c81e4806746c
odri96352 <130649579+odri96352@users.noreply.github.com>
2023-11-29 12:13:45 +0100

    Add files via upload

--- string.h
+++ string.h
@@ -11,11 +11,18 @@ class string{
     char* c_str();
     void clear();
     int max_size();
-    void operator_egal(char t);
+    string& operator=(char* t);
+    string& operator= (const string& str);
     void resize(int n);
     void resize(int n,  char c);
+
 
   private :
     char* list_char_;
     int size_;
 };
+
+
+// Remarques:
+// pour la fonction "resize", est ce qu'on peut avoir des arguments non obligatoires en c++ (pour l'arg "char c") ?
+// faire gaffe au deletes et aux news, vérifier qu'il n'y a pas de leaks
